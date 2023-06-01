80e43f4cafba98e147a2be687db65f8b809a552f
laluprasath <laluprasath.dev.in@gmail.com>
2023-06-01 14:56:27 +0530

    BOOK_CRUD

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const helmet = require("helmet");
+const compression = require("compression");
 const connectDb = require("./src/db/dbConfig");
 const authRoutes = require("./src/routes/authRoutes");
 const adminRouter = require("./src/routes/adminRouter");
@@ -7,8 +9,11 @@ const adminRouter = require("./src/routes/adminRouter");
 const app = express();
 const port = process.env.PORT;
 connectDb();
+app.use(helmet());
+app.use(compression());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRoutes);
 app.use("/books", adminRouter);
